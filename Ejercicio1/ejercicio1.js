78e4c5180f4181b3fbe74a60ecf9d72758f56cab
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 19:58:54 -0500

    :tada: segundo commit

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,4 @@
+let gradosInput = prompt ("Escriba los grados en Celsius");
+let gradosC = (Number(gradosInput) * (9/5) + 32)
+
+alert("Los grados en fahrenheit son: " + gradosC + "°F");
--- corte2.js
+++ corte2.js
@@ -5,13 +5,29 @@ let edadFutura = Number(age) + 10;
 let edadPasada = Number(age) - 5;
 let edadMult = Number(age) *3;
 let edadDob = Number(age) /2;
+// let parImpar = 
 
 //alert("Hola");
 alert("Hola " + name);
-alert( name + " en diez años su edad será: " + edadFutura);
+alert(name + " en diez años su edad será: " + edadFutura);
 alert(name + " hace cinco años su edad era: " + edadPasada);
-alert("El triple de su edad es: " + edadMult);
-alert("La mitad de su edad es: " + edadDob);
+alert(name + " el triple de su edad es: " + edadMult);
+alert(name + " la mitad de su edad es: " + edadDob);
+
+
+if (Number(age) == 0){
+alert(name + " su edad es: " + "impar");
+    }
+    else{
+        alert(name + " su edad es: " + "par");
+    };
+
+if (Number(age) >= 22) {
+    alert(name + " usted es mayor de 22 años"); 
+    }   
+        else{
+            alert(name + " usted es menor de 22 años");   
+        };
 document.write("Escuela de Artes y Letras");
 console.log("Martes 13");
-document.getElementById("cuadro1").innerHTML = nombre;
+document.getElementById("cuadro1").innerHTML = nombre; 
