4f2ba46c87c1adfdfcf60db1aa6eec5d07868ce7
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 19:35:13 -0500

    🎉 Primer Commit

--- corte2.js
+++ corte2.js
@@ -3,10 +3,15 @@ let name = prompt ("Por favor escriba su nombre");
 let age = prompt ("Escriba su edad");
 let edadFutura = Number(age) + 10;
 let edadPasada = Number(age) - 5;
+let edadMult = Number(age) *3;
+let edadDob = Number(age) /2;
+
 //alert("Hola");
 alert("Hola " + name);
-alert( name + " en diez años su edad será: " + edadFutura)
+alert( name + " en diez años su edad será: " + edadFutura);
 alert(name + " hace cinco años su edad era: " + edadPasada);
+alert("El triple de su edad es: " + edadMult);
+alert("La mitad de su edad es: " + edadDob);
 document.write("Escuela de Artes y Letras");
 console.log("Martes 13");
 document.getElementById("cuadro1").innerHTML = nombre;
