d6ff4f7ffcde454154349d4770a2a41bc278eae6
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-10-04 18:42:17 -0500

    :tada: Anexo ejercicio 3

---
+++ Ejercicio3/arregloStrings.js
@@ -0,0 +1,6 @@
+let animales = ["jirafa","ballena","gato","elefante","gato","perro","mono","murciélago","boa","canguro","flamingo","cacatua","zebra","antílope","león marino","iguana","liebre","conejo","pingüino","ocelote"];
+
+for (let i=0;i<animales.length;i++) {
+
+
+}
